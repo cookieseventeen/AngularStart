701a17d8056df6cac767c77c9e2ad497396f3697
cookieseventeen <cookie_seventeen@msn.com>
2022-11-06 21:14:16 +0800

    add scss change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,74 +1,9 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  VERSION,
-  ViewChild,
-} from '@angular/core';
-import { fromEvent, Observable } from 'rxjs';
-import { takeUntil, takeWhile, skipUntil, tap, map } from 'rxjs/operators';
+import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
-
-  @ViewChild('dragArea') dragArea: ElementRef<HTMLElement>;
-
-  public mouseDown: Observable<any>;
-  public mouseUp: Observable<any>;
-  public mouseMove: Observable<any>;
-  public press: boolean = false;
-  public moveTemp: number;
-  public moveingX: number;
-
-  ngOnInit(): void {
-    return;
-  }
-  ngAfterViewInit(): void {
-    this.mouseDown = fromEvent(this.dragArea.nativeElement, 'mousedown');
-    this.mouseUp = fromEvent(this.dragArea.nativeElement, 'mouseup');
-    this.mouseMove = fromEvent(this.dragArea.nativeElement, 'mousemove');
-
-    const source = this.mouseMove
-      .pipe(
-        tap((event) => {
-          if (this.press) {
-            if (Math.abs(this.moveTemp - event.clientX) < 2) return;
-
-            if (this.moveTemp - event.clientX > 1) {
-              console.log('scroll left');
-              this.dragArea.nativeElement.scrollLeft -= 20;
-            } else {
-              console.log('scroll right');
-              this.dragArea.nativeElement.scrollLeft += 20;
-            }
-            this.moveTemp = event.clientX;
-          }
-        })
-      )
-      .subscribe((event) => {});
-
-    this.mouseDown
-      .pipe(
-        tap((event) => {
-          this.press = true;
-          this.moveTemp = event.clientX;
-        })
-      )
-      .subscribe();
-
-    this.mouseUp
-      .pipe(
-        tap((event) => {
-          this.press = false;
-        })
-      )
-      .subscribe();
-
-    const mousedownIng = this.mouseDown;
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import { DragComponent } from './components/drag/drag.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, DragComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
